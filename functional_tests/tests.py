2d6bcb3a7aa7dee7942c36bc335b2f6b10858896
jyoti chetry <jyotichetry087@gmail.com>
2025-05-06 15:59:21 +0530

    first layout and styling FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -106,5 +106,33 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME,"body").text
         self.assertNotIn("Buy peacock feathers",page_text)
         self.assertIn("Buy milk",page_text)
+    
+    
+    def test_layout_and_styling(self):
+        # goes to url
+        self.browser.get(self.live_server_url)
+
+        # browser window is set to very specific size
+        self.browser.set_window_size(1024,768)
+
+        # input box should be in center
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+        
+        # starts a new list and the input box should be center here also
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID,"id_new_item")
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
         
         
