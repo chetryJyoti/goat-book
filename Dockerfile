3c7f7be65c6a44cb36242a6bef22416da64647e9
jyoti chetry <jyotichetry087@gmail.com>
2025-05-06 22:00:04 +0530

    Add collectstatic to dockerfile,and new location to gitignore

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,17 @@ FROM python:3.13-slim
 RUN python -m venv /venv 
 ENV PATH="/venv/bin:$PATH"
 
-RUN pip install "django<6" gunicorn whitenoise
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
 COPY src /src
 
 WORKDIR /src
+
+RUN python manage.py collectstatic
+
+ENV DJANGO_DEBUG_FALSE=1
+
 
 # RUN python manage.py migrate --noinput
 
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl(#f4ack$m^cez10*y#b!ua=+y=cyg!5rjv7!damppu&c9()t'
+# SECRET_KEY = 'django-insecure-sl(#f4ack$m^cez10*y#b!ua=+y=cyg!5rjv7!damppu&c9()t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = "insecure-key-for-dev"
+    ALLOWED_HOSTS=[]
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
